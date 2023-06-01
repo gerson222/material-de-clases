b9775c10fe65db86b8937f031ef748c44f9f17b5
manolito <gersonquiroz66@gmail.com>
2023-05-31 23:07:40 -0300

    solo menu tiene 404

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,5 +1,5 @@
 const currentLocation = window.location.pathname;
-const menuPath = currentLocation.endsWith('/') ? 'pages/menu.html' : './pages/menu.html';
+const menuPath = currentLocation.endsWith('/') ? 'pages/menu.html' : '../pages/menu.html';
 
 fetch(menuPath)
   .then((response) => response.text())
