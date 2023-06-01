0ae3ba148b1ebb23498dbe0832bc7b4104a67a0f
manolito <gersonquiroz66@gmail.com>
2023-05-31 23:16:23 -0300

    intento 2 error 404

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,6 +1,10 @@
 const currentLocation = window.location.pathname;
-const basePath = '/prueba/'; // Ruta base del proyecto
-const menuPath = currentLocation.endsWith(basePath) ? `${basePath}pages/menu.html` : `${basePath}pages/menu.html`;
+
+// Obtener la ruta base del proyecto
+const basePath = currentLocation.endsWith('/') ? currentLocation : currentLocation + '/';
+
+// Construir la ruta relativa al archivo "menu.html" según la ubicación actual
+const menuPath = basePath + 'pages/menu.html';
 
 fetch(menuPath)
   .then((response) => response.text())
