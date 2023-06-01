0bbbd39fbe9ecdf79d22a976b9d375b4ef6a869a
manolito <gersonquiroz66@gmail.com>
2023-05-31 22:17:10 -0300

    reordenar

--- scripts/constructores.js
+++ scripts/constructores.js
@@ -24,41 +24,62 @@ var padre = document.getElementById("id_padre")
 msjAlert("texto", padre, "prueba id")
 
 
-function registro(){
-    var user = [
-        document.getElementById("username").value,
-        document.getElementById("useremail").value,
-        document.getElementById("inputPassword5").value,
-        document.getElementById("inputPassword6").value,
-    ]
+function registro() {
+  var user = {
+      username: document.getElementById("username").value,
+      useremail: document.getElementById("useremail").value,
+      password: document.getElementById("inputPassword5").value,
+      confirmPassword: document.getElementById("inputPassword6").value
+  };
 
+  var tieneLetras = /[a-zA-Z]/.test(user.password);
+  var tieneNumeros = /\d/.test(user.password);
 
-    var tieneLetras = /[a-zA-Z]/.test(user[2]); // Comprueba si contiene letras
-    var tieneNumeros = /\d/.test(user[2]); // Comprueba si contiene números
+  var padreMensaje1 = document.getElementById("password");
+  if (tieneNumeros && tieneLetras) {
+      padreMensaje1.style.color = "green";
+      padreMensaje1.innerHTML = "<h5>Es válido</h5>";
+  } else {
+      padreMensaje1.style.color = "red";
+      padreMensaje1.innerHTML = "No es válido";
+  }
 
-    var padreMensaje1 = document.getElementById("password")
-    if (tieneNumeros==true && tieneLetras==true){
-        // var mensaje = document.createTextNode("las contraseña es valida")
-        padreMensaje1.style.color = "green";
-        padreMensaje1.innerHTML = "<h5> es valido </h5>";
-    } else {
-        // var mensaje = document.createTextNode("las contraseña NO ES VALIDA")
-        padreMensaje1.style.color = "red";
-        padreMensaje1.innerHTML = "es no valido";
-    }
-
-    var padreMensaje2 = document.getElementById("password2")
-    if(user[2]===user[3]){
-        // var mensaje = document.createTextNode("las contraseñas coinciden")
-        padreMensaje2.style.color = "green";
-        padreMensaje2.innerHTML = "es valido";
-    }else {
-        // var mensaje = document.createTextNode("las contraseñas NO coinciden")
-        padreMensaje2.style.color = "red";
-        padreMensaje2.innerHTML = "es no valido";
-    }
-
+  var padreMensaje2 = document.getElementById("password2");
+  if (user.password === user.confirmPassword) {
+      padreMensaje2.style.color = "green";
+      padreMensaje2.innerHTML = "Es válido";
+      // Guardar los datos en un archivo JSON
+      var jsonData = JSON.stringify(user);
+      guardarEnArchivo(jsonData); // Esta función guarda los datos en un archivo
+  } else {
+      padreMensaje2.style.color = "red";
+      padreMensaje2.innerHTML = "No es válido";
+  }
 }
+
+function guardarEnArchivo(data) {
+  var jsonData = JSON.stringify(data);
+  var blob = new Blob([jsonData], { type: "application/json" });
+
+  // Definir la ruta relativa del archivo JSON
+  var jsonFilePath = '../json/usuario.json';
+
+  // Crear un enlace de descarga
+  var a = document.createElement("a");
+  a.href = URL.createObjectURL(blob);
+  a.download = jsonFilePath;
+
+  // Agregar el enlace de descarga al documento
+  document.body.appendChild(a);
+
+  // Hacer clic en el enlace para iniciar la descarga del archivo
+  a.click();
+
+  // Eliminar el enlace del documento
+  document.body.removeChild(a);
+}
+
+
 
 function tema() {
   var elementos = document.body.getElementsByTagName("*");
